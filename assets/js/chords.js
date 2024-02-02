68662a8930bbdefe14dd5bb54f4346e1f2b071a0
Guilherme I F L Weizenmann <g.weizenmann@gmail.com>
2024-02-02 14:47:02 -0300

    Update chords

--- assets/js/chords.js
+++ assets/js/chords.js
@@ -24,10 +24,11 @@ if (chords) {
         }
         blanks = 0;
         lastWasSection = false;
+        console.log('line', text)
 
-        if (/^ *\[[A-Za-zÀ-ÖØ-öø-ÿ0-9?{} -]+\]$/.test(text)) {
+        if (/^ *\[[A-Za-zÀ-ÖØ-öø-ÿ0-9?{} -]+\] *$/.test(text)) {
             lastWasSection = true;
-            // console.log('section', text)
+            console.log('section', text)
             sections++;
             if (section.children.length > 0) {
                 chords.appendChild(section);
@@ -38,13 +39,14 @@ if (chords) {
             section.dataset.name = line.textContent.substring(1, line.textContent.length - 1);
             line.classList.add("has-text-weight-bold");
             line.classList.add("my-5");
-        } else if (/^[A-H1-9Mm#bdisu°+\%()*~v^| ><\[\]/ mpf]+$/.test(text)) {
-            // console.log('chords', text)
+        } else if (/^[A-H1-9Mm#bdisu°+\%()*~v^| ><\[\]/ mpf-]+$/.test(text)) {
+            console.log('chords', text)
             //line.innerHTML = text.replace('v', '&#x2193;').replace('^', '&#x2191;')
             line.classList.add("mt-2");
             line.classList.add("has-text-primary");
             line.classList.add("has-text-weight-bold");
         }
+        console.log("end", line)
         section.appendChild(line);
     }
 
