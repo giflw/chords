e3160857d7f16a2bf4b0ea840753fd845cca987e
Guilherme I F L Weizenmann <g.weizenmann@gmail.com>
2024-02-26 21:40:57 -0300

    Add app-shell

---
+++ chordapp/src/app/app-shell/app-shell.component.ts
@@ -0,0 +1,16 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'itq-app-shell',
+  standalone: true,
+  imports: [],
+  template: `
+    <p>
+      app-shell works!
+    </p>
+  `,
+  styles: ``
+})
+export class AppShellComponent {
+
+}
